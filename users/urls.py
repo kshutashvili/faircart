88d58afb9e076ccb175c11881a5df23e54f0391e
Pavel Koryakin <xtr72@yandex.ru>
2016-07-20 17:06:25 +0300

    Add password change

--- users/urls.py
+++ users/urls.py
@@ -12,5 +12,11 @@ urlpatterns = [
         kwargs={'next_page': '/'}),
     url(r'^create/$', RegView.as_view(), name='registration'),
     url(r'^verify/(?P<ctype>(%s))/$' % '|'.join(ContactVerification.TYPE._ALL),
-        VerifyContactView.as_view(), name='verify_contact')
+        VerifyContactView.as_view(), name='verify_contact'),
+    url(r'^password/change/$', auth_views.password_change,
+        kwargs={'template_name': 'users/password_change.html'},
+        name='password_change'),
+    url(r'^password/change/done/$', auth_views.password_change_done,
+        kwargs={'template_name': 'users/password_change_done.html'},
+        name='password_change_done'),
 ]
