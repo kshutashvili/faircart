65f3e15df716a3d08e17906f94bbb8983abc17af
SergioAnd18 <developinc@yandex.ru>
2016-08-12 02:11:00 +0300

    remove ajax from form send

--- main/static/js/ui.js
+++ main/static/js/ui.js
@@ -2,17 +2,39 @@
  * Created by batman on 10.08.16.
  */
 (function($){
-  $.fn.ajaxSendForm= function () {
+
+    $.fn.serializeObject = function () {
+        var obj = new Object();
+        var arr = $(this).serializeArray();
+        for (i in arr){
+            obj[arr[i]['name']] = arr[i]['value'];
+        }
+        return obj
+  };
+
+  $.fn.ajaxSendForm = function () {
       var url = $(this).attr('action');
-      var data = $(this).serialize();
-      $.ajax({
+      var data = $(this).serializeObject();
+      var method = '';
+      if($(this).attr('method') == undefined){
+          method = 'get'
+      }
+      else{
+          method = $(this).attr('method');
+      }
+
+      $.redirect(url,data,method);
+      /*$.ajax({
           url:url,
           type:'POST',
           data: data,
-          success:function (data) {
+          success:function (data, statusText, xhr) {
               alert(data);
+              alert(xhr.getResponseHeader('href'));
+              console.log(xhr);
+              //.location.reload('/');
           }
-      });
+      });*/
 
   }
     
