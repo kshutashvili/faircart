021fe90019a1289d86f122e48ddd1852203c5198
Pavel Koryakin <xtr72@yandex.ru>
2016-08-26 16:45:56 +0300

    FC-37 Add sms to email backend

--- sms/backends/base.py
+++ sms/backends/base.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import get_user_model
+
+
 class BaseBackend(object):
     def __init__(self, **options):
         self.default_sender = options.pop('default_sender')
@@ -20,3 +23,23 @@ class ConsoleBackend(BaseBackend):
 class NullBackend(BaseBackend):
     def send(self, msg, to, sender=None):
         pass
+
+
+class EmailBackend(BaseBackend):
+    def __init__(self, **options):
+        super(EmailBackend, self).__init__(**options)
+        # We use property to avoid import problems
+        self._user_model = None
+
+    @property
+    def user_model(self):
+        if self._user_model is None:
+            self._user_model = get_user_model()
+        return self._user_model
+
+    def send(self, msg, to, sender=None):
+        try:
+            user = self.user_model.objects.get(phone=to)
+        except self.user_model.DoesNotExist:
+            return
+        user.email_user('SMS message', msg)
